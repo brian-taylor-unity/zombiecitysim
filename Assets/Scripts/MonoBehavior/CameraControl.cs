2103b6d435ffde3a5fdc41b0444ede1b3851667d
Brian Taylor <taylor.brian.eric@gmail.com>
2019-01-21 15:55:14 -0500

    First pass at implementing camera controls (orbit-zoom style)

---
+++ Assets/Scripts/MonoBehavior/CameraControl.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float stickMinZoom;
+    public float stickMaxZoom;
+    public float swivelMinZoom;
+    public float swivelMaxZoom;
+    public float rotationSpeed;
+    public float moveSpeedMinZoom;
+    public float moveSpeedMaxZoom;
+
+    private Transform swivel;
+    private Transform stick;
+    
+    private float zoom = 1f;
+    private float rotationAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swivel = transform.GetChild(0);
+        stick = swivel.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (zoomDelta != 0f)
+            AdjustZoom(zoomDelta);
+
+        float rotationDelta = Input.GetAxis("Rotation");
+        if (rotationDelta != 0f)
+            AdjustRotation(rotationDelta);
+
+        float xDelta = Input.GetAxis("Horizontal");
+        float zDelta = Input.GetAxis("Vertical");
+        if (xDelta != 0f || zDelta != 0f)
+            AdjustPosition(xDelta, zDelta);
+    }
+
+    private void AdjustZoom(float delta)
+    {
+        zoom = Mathf.Clamp01(zoom + delta);
+        float distance = Mathf.Lerp(stickMinZoom, stickMaxZoom, zoom);
+        stick.localPosition = new Vector3(0f, 0f, distance);
+
+        float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
+        swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
+    }
+
+    private void AdjustRotation(float delta)
+    {
+        Debug.Log("rotate");
+        rotationAngle += delta * rotationSpeed * Time.deltaTime;
+        if (rotationAngle < 0f)
+            rotationAngle += 360f;
+        if (rotationAngle >= 360f)
+            rotationAngle -= 360f;
+
+        transform.localRotation = Quaternion.Euler(0f, rotationAngle, 0f);
+    }
+
+    private void AdjustPosition(float xDelta, float zDelta)
+    {
+        Vector3 direction = transform.localRotation * new Vector3(xDelta, 0f, zDelta).normalized;
+        float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
+        float distance = Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom) * damping * Time.deltaTime;
+
+        Vector3 position = transform.localPosition;
+        position += direction * distance;
+        transform.localPosition = position;
+    }
+}
