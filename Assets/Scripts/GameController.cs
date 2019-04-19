d6cb8e44b7d9a72d4c2cd9f76d1238fad6ffc27c
Brian Taylor <taylor.brian.eric@gmail.com>
2019-04-18 21:29:24 -0400

    UI elements are hooked up and working for Human and Zombie TurnDelay as well as TurnDelayTime. Values can be actively changed during runtime.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,12 +10,21 @@ public class GameController : MonoBehaviour
     public int numStreets = 30;
     public int numHumans = 1000;
     public int numZombies = 1;
-    public float turnDelayTime = 1.0f;
+    public int humanTurnDelay = 1;
+    public int zombieTurnDelay = 3;
+    public float turnDelayTime = 0.025f;
 
     public Slider numHumansSlider;
     public InputField numHumansInputField;
     public Slider numZombiesSlider;
     public InputField numZombiesInputField;
+    public InputField numTilesXInputField;
+    public InputField numTilesYInputField;
+    public InputField streetsInputField;
+
+    public InputField humanTurnDelayInputField;
+    public InputField zombieTurnDelayInputField;
+    public InputField turnDelayTimeInputField;
 
     void Awake()
     {
@@ -26,15 +35,36 @@ public class GameController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        numTilesXInputField.text = numTilesX.ToString();
+        numTilesYInputField.text = numTilesY.ToString();
+        streetsInputField.text = numTilesY.ToString();
         numHumansSlider.value = numHumans;
         numHumansInputField.text = numHumans.ToString();
         numZombiesSlider.value = numZombies;
         numZombiesInputField.text = numZombies.ToString();
+        humanTurnDelayInputField.text = humanTurnDelay.ToString();
+        zombieTurnDelayInputField.text = zombieTurnDelay.ToString();
+        turnDelayTimeInputField.text = (turnDelayTime * 1000).ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    public void SetNumTilesXInputField(string num)
+    {
+        numTilesX = int.Parse(num);
+    }
+
+    public void SetNumTilesYInputField(string num)
+    {
+        numTilesY = int.Parse(num);
+    }
+
+    public void SetNumStreetsInputField(string num)
+    {
+        numStreets = int.Parse(num);
     }
 
     public void SetNumHumansSlider(float num)
@@ -59,5 +89,20 @@ public class GameController : MonoBehaviour
     {
         numZombies = int.Parse(num);
         numZombiesSlider.value = numZombies;
+    }
+
+    public void SetHumanTurnDelay(string num)
+    {
+        humanTurnDelay = int.Parse(num);
+    }
+
+    public void SetZombieTurnDelay(string num)
+    {
+        zombieTurnDelay = int.Parse(num);
+    }
+
+    public void SetTurnDelayTime(string num)
+    {
+        turnDelayTime = float.Parse(num) / 1000;
     }
 }
